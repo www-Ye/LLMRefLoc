973813e8d8d40f2cee595c3a7ff95005deb8c6cc
www-Ye <919872507@qq.com>
2024-02-16 16:42:16 +0800

    commit

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python src/llm2gr.py --dataset nq \
+python src/llmrefloc.py --dataset nq \
 	--inference_mode recite --model_path llama-2-13b \
 	--stage1_beam_size 15 --stage2_beam_size 10 --stage1_return_nums 2 \
 	--prefix_len 16 --lam 0.9
